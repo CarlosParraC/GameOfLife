446d4d27be5717d5eb976c8743f29e4a1b4fd4fb
CarlosParraC <carlos.parra.ch@gmail.com>
2023-02-06 14:37:47 -0600

    Add cell size var; Correction of board limits

--- game.py
+++ game.py
@@ -2,10 +2,11 @@ import time
 import pygame
 import numpy as np
 
-color_background = (0,0,0)
-color_grid = (10,10,10)
+color_background = (0, 0, 0)
+color_grid = (40, 40, 40)
 color_dying = (170, 170, 170)
-color_alive = (255,255,255)
+color_alive = (255, 255, 255)
+size = (80, 80)
 
 def game_logic(board, posX, posY, neighboursAlive):
     if (board[posX, posY] == 1 ):
@@ -22,7 +23,7 @@ def update(screen, board, size, progress=False):
     board_new = np.zeros((board.shape[0], board.shape[1]))
 
     for (row, col) in np.ndindex(board.shape):
-        neighboursAlive = np.sum(board[row-1:row+2, col-1:col+2]) - board[row, col]
+        neighboursAlive = np.sum(board[row - 1 : row + 2, col - 1 : col + 2]) - board[row, col]
         color = color_background if board[row,col] == 0 else color_alive
 
         if (progress):
@@ -30,16 +31,16 @@ def update(screen, board, size, progress=False):
             if (result):
                 board_new[row, col], color = result
 
-        pygame.draw.rect(screen, color, (col*size, row*size, size-1, size-1))
+        pygame.draw.rect(screen, color, (col * size, row * size, size - 1, size - 1))
     
     return board_new
 
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((800, 800))
+    screen = pygame.display.set_mode((size[0] * 10, size[1] * 10))
 
-    cells = np.zeros((80, 80))
+    cells = np.zeros(size)
     screen.fill(color_grid)
     update(screen, cells, 10)
 
@@ -58,16 +59,18 @@ def main():
                     update(screen, cells, 10)
                     pygame.display.update()
                 if(event.key == pygame.K_BACKSPACE):
-                    cells = np.zeros((80, 80))
+                    cells = np.zeros(size)
                     update(screen, cells, 10)
                     pygame.display.update()
             
             if (pygame.mouse.get_pressed()[0]):
                 pos = pygame.mouse.get_pos()
-                cells[pos[1] // 10, pos[0] // 10] = 1
-                update(screen, cells, 10)
-                pygame.display.update()
-
+                print(pos)
+                if (pos[0] > 0 and pos[0] < 800):
+                    if (pos[1] > 0 and pos[1] < 800):
+                        cells[pos[1] // 10, pos[0] // 10] = 1
+                        update(screen, cells, 10)
+                        pygame.display.update()
 
         screen.fill(color_grid)
 
